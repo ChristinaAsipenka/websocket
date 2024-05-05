372726fccca8075724fd2dcd8022457e073a5769
Christina Asipenka <krystinabutko@gmail.com>
2024-05-05 13:20:00 +0200

    init vue

---
+++ resources/js/Pages/Message/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-1/2 mx-auto py-6">
+        Message
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
